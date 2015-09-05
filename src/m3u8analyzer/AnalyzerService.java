179253b3a1a9b8aa9be0432fce40438f4586553a
Cory Steeley <cjacksteel@gmail.com>
2015-09-05 17:18:34 -0400

    moved download utilties to their own class, created ability to download subfiles

--- src/m3u8analyzer/AnalyzerService.java
+++ src/m3u8analyzer/AnalyzerService.java
@@ -1,45 +1,19 @@
 package m3u8analyzer;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 
 public class AnalyzerService {
 
 	public static void main(String[] args) throws IOException {
 		
-		URL website = null;
-		try {
-			website = new URL("http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8");
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		URL website = new URL("http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8");
 		
-		ReadableByteChannel rbc = null;
-		try {
-			rbc = Channels.newChannel(website.openStream());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		FileOutputStream fos = null;
-		String fileName = null;
+		FileFetcher ff = new FileFetcher();
+		ff.downloadMasterFile(website);
 		
-		try {
-			fileName = website.toString().substring(website.toString().lastIndexOf('/') + 1);
-			System.out.println(fileName);
-			fos = new FileOutputStream(fileName);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-		System.out.println("Finished.");
+		File masterFile = new File(ff.getMasterFileLocation(website));
+		ff.downloadChildFiles(website, masterFile);
 	}
-
 }
---
+++ src/m3u8analyzer/FileFetcher.java
@@ -0,0 +1,111 @@
+package m3u8analyzer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class FileFetcher {
+	
+	//get the master m3u8 file and store it in the m3u8 files directory of the project
+	public void downloadMasterFile(URL website) throws IOException{
+		String fileName = null;
+		FileOutputStream fos = null;
+		
+		ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+				
+		File outputDirectory = new File("m3u8 files/" + website.toString().replaceAll("[-+.^:,/]","_") );
+		if (!outputDirectory.exists()) {
+			if (outputDirectory.mkdir()) {
+				System.out.println("Directory " + outputDirectory + " is created!");
+			} else {
+				System.out.println("Failed to create directory!");
+			}
+		}
+		
+		try {
+			fileName = outputDirectory + "/" + website.toString().substring(website.toString().lastIndexOf('/') + 1);
+			fos = new FileOutputStream(fileName);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+	}
+	
+	//take in a master file and download child m3u8 files
+	public void downloadChildFiles(URL website, File masterFile) throws IOException{
+		URL childAddress;
+ 	   	String line;
+ 	   	String urlDifference;
+ 	   	FileOutputStream fos = null;
+		String fileName = null;
+		File outputDirectory;
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(masterFile))) {
+		    while ((line = br.readLine()) != null) {
+		       if(line.startsWith("#")){
+		    	   //do nothing
+		       }
+		       else{
+		    	   //if the file is using full URL just use the line
+		    	   if(line.startsWith("http://")){
+		    		   childAddress = new URL(line);
+		    	   }
+		    	   //if file is using relative URL, combine it with the full URL of the master
+		    	   else{
+		    		   childAddress = new URL(website.toString().substring(0, website.toString().lastIndexOf('/') + 1) + line);
+		    	   }
+		   
+		    	   //Check if it's in a different directory  on the web server than the master file
+		    	   urlDifference = StringUtils.difference(website.toString(), childAddress.toString());
+		    	   
+		    	   //If it is, make the directory locally
+		    	   if(urlDifference.contains("/")){
+		    		   outputDirectory = new File("m3u8 files/" + website.toString().replaceAll("[-+.^:,/]","_") + "/" + urlDifference.substring(0, urlDifference.lastIndexOf("/") + 1));
+		    			if (!outputDirectory.exists()) {
+		    				if (outputDirectory.mkdir()) {
+		    					System.out.println("Directory " + outputDirectory + " is created!");
+		    				} else {
+		    					System.out.println("Failed to create directory!");
+		    				}
+		    			}
+		    	   }
+		    	   else{
+		    		   outputDirectory = new File("m3u8 files/" + website.toString().replaceAll("[-+.^:,/]","_") );
+		    	   }
+		    	   
+		    	   ReadableByteChannel rbc = Channels.newChannel(childAddress.openStream());
+		    	   
+		    	   try {
+		   				fileName = outputDirectory + "/" + childAddress.toString().substring(childAddress.toString().lastIndexOf('/') + 1);
+		   				fos = new FileOutputStream(fileName);
+		   			} catch (FileNotFoundException e) {
+		   			// TODO Auto-generated catch block
+		   				e.printStackTrace();
+		   			}
+		   			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+  
+		       }
+		    }
+		}
+	}
+	
+	public String getMasterFileLocation(URL website){
+		String path = "m3u8 files/" + website.toString().replaceAll("[-+.^:,/]","_") + "/" + website.toString().substring(website.toString().lastIndexOf('/') + 1);
+		File f = new File(path);
+		if(f.exists() && !f.isDirectory()) {
+			return path;
+		}
+		else{
+			return "File does not exist!";
+		}
+	}
+}
