b336355b77ab150a7d51f84f0287a17e11f30aa5
Cory Steeley <cjacksteel@gmail.com>
2015-09-09 10:07:18 -0400

    Converted to read from a conf file for the URL

--- src/m3u8analyzer/AnalyzerService.java
+++ src/m3u8analyzer/AnalyzerService.java
@@ -1,14 +1,36 @@
 package m3u8analyzer;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 
 public class AnalyzerService {
 
 	public static void main(String[] args) throws IOException {
+		String address;
 		
-		URL website = new URL("http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8");
+		//read in conf file with the address
+		BufferedReader br = new BufferedReader(new FileReader("conf/address.conf"));
+		try {
+		    StringBuilder sb = new StringBuilder();
+		    String line = br.readLine();
+
+		    while (line != null) {
+		    	if(line.startsWith("http://")){
+			    	sb.append(line);
+			        sb.append(System.lineSeparator());
+			    }
+		        line = br.readLine();
+		    }
+		    	address = sb.toString();
+		} finally {
+		    br.close();
+		}
+		
+		System.out.println(address);
+		URL website = new URL(address);
 		
 		FileFetcher ff = new FileFetcher();
 		ff.downloadMasterFile(website);
